0a1f144408183f25adcda3147ece194e5a385ab6
Szabus <szendroi.szabina@gmail.com>
2021-09-03 18:52:47 +0200

    gollum

---
+++ src/main/model/humanoids/Gollum.java
@@ -0,0 +1,10 @@
+package main.model.humanoids;
+
+import main.model.animals.Animal;
+
+public class Gollum extends Humanoid{
+
+    public void renamePet(Animal animal, String newName) {
+        animal.setName("my precious" + newName);
+    }
+}
