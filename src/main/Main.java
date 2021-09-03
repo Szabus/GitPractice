b92dad8ef015bcdaaef6304d2518caa0e3741d06
sumflo <real61madrid@gmail.com>
2021-09-03 18:58:22 +0200

    ursula

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,24 @@
 package main;
 
+import main.model.animals.Animal;
+import main.model.animals.Cat;
+import main.model.animals.Dog;
+import main.model.animals.RoseLamb;
+import main.model.humanoids.Human;
+
 public class Main {
 
     public static void main(String[] args) {
-        // TODO
+
+        Human ursula = new Human();
+
+        Animal cat = new Cat();
+        Animal dog = new Dog();
+        Animal lamb = new RoseLamb();
+
+        ursula.getPetsOwned().add(cat);
+        ursula.getPetsOwned().add(dog);
+        ursula.getPetsOwned().add(lamb);
+
     }
 }
